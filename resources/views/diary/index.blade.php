2d173edb61186ff1d2564f8ac42b101b021bfc87
L-min <wizard0948@gmail.com>
2019-06-20 09:54:40 +0900

    second commit

--- resources/views/diary/index.blade.php
+++ resources/views/diary/index.blade.php
@@ -1,33 +1,86 @@
 @extends('layouts.default')
 @section('content')
-    <h2>My diary page</h2>
+  <head>
+    <div style="background-image : url(./img/diary_background.png)">
+    <p class="font-italic" style="font-size:30px;"><i class="fas fa-bars"></i>&nbsp;My diary page&nbsp;<i class="fas fa-bars"></i></p>
+      <?php //echo url('/img/diary_background.png'); ?>
     <br>
-    <button type="button" onclick="location.href='/mindiary/new'">Create</button><br>
-    <br>
-    @foreach($diary->reverse() as $temp)
-    <h4>No. {{ $temp->id }}</h4>
-    <div class = "diary_body">
-      <div class="card">
-        <div class="card-header diary_header">Title : {{ $temp->title }} </div>
-        <div class="card-body" style="height : 200px" >Content : <br><br> <pre style="font-size : 15px">{{ $temp->content }} </pre></div>
-        <div class="card-footer">Data : {{ $temp->updated_at }} </div>
-      </div>
-        <button type="button" onclick="location.href='/mindiary/edit/{{$temp->id}}'">Edit</button>
-        <button type="button" onclick="location.href='/mindiary/{{$temp->id}}'">Delete</button>
-    </div>
-    <br>
-    @endforeach
-    </br>
-    <div>
-      <button type="button" onclick="location.href='/mindiary/new'">Create</button>
-    </div>
 
+
+    <button type="button" class="btn btn-dark" onclick="location.href='/mindiary/new'">Create</button><br>
+    <br>
+  </div>
+  </head>
+    <div class="body_flex" style="background-color : #FEBEBE">
+      <div class="body_left">
+      @foreach($diary->reverse() as $temp)
+        <div class="bg-primary"><i class="fas fa-book"></i>&nbsp;No. {{ $temp->id }}&nbsp;<i class="fas fa-book"></i></div>
+      <div class = "diary_body">
+        <div class="card">
+          <div class="card-header diary_header"><span class="badge badge-primary" style="font-size:15px"> Title </span> {{ $temp->title }} </div>
+          <div class="card-body"><span class="badge badge-primary" style="font-size:15px"> Content </span> <br><br> <pre style="font-size : 15px">{{ $temp->content }} </span></pre></div>
+          <div class="card-footer"><span class="badge badge-primary" style="font-size:15px"> Data </span> {{$temp->updated_at}} </div>
+        </div>
+          <button type="button" class="btn btn-secondary" onclick="location.href='/mindiary/edit/{{$temp->id}}'">Edit</button>
+          <button type="button" class="btn btn-secondary" onclick="location.href='/mindiary/{{$temp->id}}'">Delete</button>
+        </div>
+        <br>
+        @endforeach
+      </div>
+      <div class="side_menu">
+        <div class="side_right_title">List</div>
+          <div class="side_menu_link">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/paint" target="_blank">Canvas</a></li>
+              <li><a href="https://github.com/laravel/laravel" target="_blank">Github</a></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+    <div>
+      <button type="button" class="btn btn-dark" onclick="location.href='/mindiary/new'">Create</button>
+    </div>
     <style>
+      .bg-primary {
+        width : 10%;
+        font-size : 20px;
+        border : double black;
+      }
       .diary_body {
-        width : 80%;
+        width : 100%;
+        background-color : #AFFFEE;
         border : 1px solid skyblue;
         border-radius : 4px;
         padding : 15px;
+      }
+      .card{
+        border-color : black;
+      }
+      .body_flex {
+        display:flex;
+      }
+      .body_left {
+        //border : 1px solid black;
+        padding : 2%;
+        width : 80%;
+      }
+      .side_menu {
+         width:15%;
+         border : double black;
+         margin-left : 3%;
+         margin-top : 3%;
+      }
+      .side_right_title {
+        font-size:20px;
+        font-weight:bold;
+        background-color:#C4F0EF;
+        color:#666666;
+        text-align:center;
+
+      }
+      .side_menu_link {
+        margin-top : 5%;
       }
     </style>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,11 +1,12 @@
+
 <!DOCTYPE html>
 <html>
   <head>
     <link rel="stylesheet" type="text/css" href="/css/app.css">
-  </head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
   <body>
     <div>
-      @yield('content')
+        @yield('content')
     </div>
   </body>
 </html>
